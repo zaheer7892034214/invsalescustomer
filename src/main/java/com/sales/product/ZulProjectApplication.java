a24f408fbd15ca8eed7f8c0249ab30d7fdaa0c07
zaheer9581883610@gmail.com <zaheer9581883610@gmail.com>
2019-12-25 20:18:21 +0530

    fifth commit

---
+++ src/main/java/com/sales/product/ZulProjectApplication.java
@@ -0,0 +1,17 @@
+package com.sales.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableDiscoveryClient
+@EnableZuulProxy
+@SpringBootApplication
+public class ZulProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZulProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sales/product/ZulProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sales.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZulProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
